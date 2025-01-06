51c6e3e7389aa792fcd5259c7e5171c40f7cc967
wdy <wdy@pro.com>
2025-01-06 17:43:50 +0800

    feat：暂存

--- micro/src/main.js
+++ micro/src/main.js
@@ -1,10 +1,31 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
+const dataListener = (data) => {
+  try {
+    console.error('dataListener', data)
+    if (data.token) {
+      sessionStorage.setItem(TOKEN, data.token)
+      store.commit('SET_TOKEN', data.token)
+    }
+    if (data.baseURL) {
+      store.commit('SET_SYSTEM_BASE_URL', data.baseURL)
+      sessionStorage.setItem(BASE_REQUEST_URL, data.baseURL)
+    }
+    if (data.microAppId) {
+      store.commit('SET_SYSTEM_MICRO_APP_ID', data.microAppId)
+    }
+
+    // SET_SYSTEM_MICRO_APP_ID
+    // 设置宽度
+    store.commit('SET_SYSTEM_COLLAPSED', Boolean(data.collapsed))
+  } catch (error) {
+    console.warn(error)
+  }
+}
 
 const app = createApp(App)
 
